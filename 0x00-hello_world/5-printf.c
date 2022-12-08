746d117c6040145f1704ee1d5bfc31bc4f5a1229
Melaku <melakubirhanu@gmail.com>
2022-12-08 12:37:25 -0800

    brr

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	char msg "\"programming is like building a multilingual puzzle\n";
-		puts(msg);
+	printf("programming is like building a multilingual puzzle\n");
+		
 				return(0);
 				}
