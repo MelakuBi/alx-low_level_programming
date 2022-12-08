5707b56c2f524de62deb6978472bd7ce20ae84c1
Melaku <melakubirhanu@gmail.com>
2022-12-08 14:45:31 -0800

    d

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/*
+ * comments
+ * goes
+ * here
+ */
+int main(void)
+{
+	write(1, "and the piece of art is usiful\" - dora korpar, 2015-10-19\n", 59);
+	return (1);
+}
