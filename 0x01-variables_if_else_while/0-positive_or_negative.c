73276b58a89ae53c679020c4a0e4bbcd29955e7f
Melaku <melakubirhanu@gmail.com>
2022-12-09 09:48:15 -0800

    $msg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,10 +10,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 if (n > 0)
-	printf('n', ":is Positive\n");
+	printf(":is Positive\n");
 if (n == 0)
-	printf(n ":is zero\n");
+	printf(":is zero\n");
 else
-	printf('n' ":is negative\n");
+	printf(":is negative\n");
 	return (0);
 }
