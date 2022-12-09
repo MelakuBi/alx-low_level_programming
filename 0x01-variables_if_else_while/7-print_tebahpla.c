6f0b3bc7b7b338f38de7c5cdf50f81e6e95ebb14
Melaku <melakubirhanu@gmail.com>
2022-12-09 14:03:04 -0800

    $msg

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - start here
+ *Description: print lower case alphabet
+ *Return: always success
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'z'; alphabet = 'a'; alphabet--)
+{
+	putchar(alphabet);
+}
+	putchar("\n");
+	turn (0);
+}
