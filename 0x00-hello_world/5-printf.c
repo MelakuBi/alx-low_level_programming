0a1cd5b91ae06c9bcc87b186c1dbc2fbeabddc00
Melaku <melakubirhanu@gmail.com>
2022-12-08 23:53:57 -0800

    five

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,11 @@
 /**
  *main: void type
  *printf: text output
- *Rreturn: non integer value
+ *Return: non integer value
  */
 int main(void)
 {
 	printf("witha proper grammar, but the outcome is a piece of art,\n");
 		return (0);
-		
 		
 }
