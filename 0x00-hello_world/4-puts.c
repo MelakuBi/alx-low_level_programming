d2c4110b4007973e61a1247cd4fc3ac39ff33ff0
Melaku <melakubirhanu@gmail.com>
2022-12-09 02:18:09 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 /**
  * main - Bignning of the program
  * puts: will print from the arry
- * return: zero intiger
+ * return : zero intiger
  */
 int main(void)
 {
-	char Mystr[] = "\"programming is like building a multilingual puzzle";
+	char Mystr[] = "\"programming is like building a multilingual puzzle\n";
 	puts(Mystr);
-	return (0);
+	return 0;
 }
