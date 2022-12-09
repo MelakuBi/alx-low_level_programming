56250f223d0892469a8eaa292762c2b28c62c07d
Melaku <melakubirhanu@gmail.com>
2022-12-09 02:23:42 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char Mystr[] = "\"programming is like building a multilingual puzzle\n";
+	char Mystr[] = "\"programming is like building a multilingual puzzle";
 	puts(Mystr);
-	return 0;
+	return (0);
 }
