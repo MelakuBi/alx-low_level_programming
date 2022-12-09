6c9496a6dd28c6923944965838ae5271ff143adb
Melaku <melakubirhanu@gmail.com>
2022-12-09 13:36:41 -0800

    $msg

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - is mandatory
+ * Description: is mandatory
+ *Return: is mandatory
+ *
+ */
+int main(void)
+{
+	for (int i=0; i<10; i++)
+	putchar(i);
+	return (0);
+}
