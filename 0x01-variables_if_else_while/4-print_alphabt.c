7734c4515cc96d1d762d931ebdfb4f10a7770740
Melaku <melakubirhanu@gmail.com>
2022-12-09 13:10:31 -0800

    $msg

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - start here
+ *Description: print lower case alphabet
+ *Return: always success
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+	if (alphabet != "e" || alphabet != "q")
+	putchar(alphabet);
+}
+	putchar('\n');
+	return (0);
+}
