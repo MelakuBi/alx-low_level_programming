77e0fef8538781c8ceb2fa945517c2e3b310aa31
Melaku <melakubirhanu@gmail.com>
2022-12-08 23:14:18 -0800

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
-/*
+/**
  * comments
  * goes
  * here
  */
 int main(void)
 {
-	/*
-	 * comments
-	 */
-	write(1, "and that piece of art is usiful\" - Dora korpar, 2015-10-19\n", 59);
+	 	write(1, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
 	/*
 	 * comments
 	 */
 }
-/*
- * comments
- */
