c98fe856a6cdff121e43c4b73a06bd56a58ac67f
Melaku <melakubirhanu@gmail.com>
2022-12-09 23:36:26 -0800

    $msg

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,12 +10,10 @@ int main(void)
 	char alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'd'; alphabet++)
-	putchar(alphabet);
-	/*if (alphabetd == 'e' || alphabet == 'q'*/
-	for (alphabet = 'f'; alphabet <= 'p'; alphabet++)
-	putchar(alphabet);
-	for (alphabet = 'r'; alphabet <= 'z'; alphabet++)
-	putchar(alphabet);
+{
+	if (alphabet != 'e' && alphabet == 'q')
+	putchar("%d", alphabet);
+}
 	putchar('\n');
 	return (0);
 }
