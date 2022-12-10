54d3c9c25c25f34aee8fab632526d92ed4633d24
Melaku <melakubirhanu@gmail.com>
2022-12-10 00:46:58 -0800

    $msg

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - program starts here
+ *Description: this program description
+ *Return: always return success
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+{
+	putchar(a);
+	if (n < 9)
+{
+	putchar(",");
+	putchar(" ");
+}
+}
+	putchar("\n");
+	return (0);
+}
+
