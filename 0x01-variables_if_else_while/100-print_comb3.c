ace7679d050fbae41e32520d79eca05465bacd10
Melaku <melakubirhanu@gmail.com>
2022-12-10 23:45:21 -0800

    $msg

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *main - program starts here
+ *Description: this program description
+ *putchar: to accept characters
+ *Return: always return success
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = '0'; a <= '8'; a++)
+{
+	for (b = '1'; b <= '9'; b++)
+{
+	if (b <= '9')
+{
+	putchar(a);
+	putchar(b);
+	putchar(',');
+	putchar(' ');
+}
+}
+	putchar('\n');
+	return (0);
+}
