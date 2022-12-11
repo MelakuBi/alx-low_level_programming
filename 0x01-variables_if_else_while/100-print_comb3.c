d8e2b43f2c3a24179f3156804dfa527fffb02155
Melaku <melakubirhanu@gmail.com>
2022-12-11 01:04:40 -0800

    $msg

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,18 +8,18 @@
 int main(void)
 {
 	int a;
-	int b;
+	int b = 0;
 
 	for (a = '0'; a <= '8'; a++)
 {
-	for (b = '1'; b <= '9'; ++b)
+	for (; b <= '9'; ++b)
 {
 	if (b <= '9')
 {
 	putchar(a);
 	putchar(b);
 }
-	if (b <= '9' && a <= '7')
+	if (b <= '9' && (a <= '7' && b <= '8')
 {
 	putchar(',');
 	putchar(' ');
