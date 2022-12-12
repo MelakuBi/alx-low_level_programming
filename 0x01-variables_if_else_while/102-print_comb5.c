930595d8298938b84097b5e8c7d1e944c011809b
Melaku <melakubirhanu@gmail.com>
2022-12-12 07:15:05 -0800

    $msg

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ *main - program starts here
+ *Description: this program description
+ *putchar: to accept characters
+ *Return: always return success
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = '0'; a <= '9'; a++)
+{
+	for (b = '0'; b <= '9'; b++)
+{
+	for (c = a; c <= '9'; c++)
+{
+	d = b + 1;
+	for (d = d; d <= '9'; d++)
+{
+	putchar(a);
+	putchar(b);
+	putchar(' ');
+	putchar(c);
+	putchar(d);
+	if (a <= '9' && b <= '9' && c <= '9' && d <= '9')
+	{
+	putchar(',');
+	putchar(' ');
+	}
+}
+}
+}
+}
+	putchar('\n');
+	return (0);
+}
