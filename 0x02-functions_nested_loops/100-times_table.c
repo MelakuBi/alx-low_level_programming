06e5a6550cfd37af286fb811a92479d84eee6ac7
Melaku <melakubirhanu@gmail.com>
2022-12-14 14:04:25 -0800

    $msg

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,6 +11,7 @@ void print_times_table(int n)
 	if (n >= 0 && n <= 15)
 	{
 	for (num = 0; num <= n; num++)
+	{
 	_putchar('0');
 	for (mult = 1; mult <= n; mult++)
 	{
@@ -33,5 +34,6 @@ void print_times_table(int n)
 	_putchar((prod % 10) + '0');
 	}
 	_putchar('\n');
+}
 }
 }
