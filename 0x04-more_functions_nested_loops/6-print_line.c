1169fe79a2f4092d41df51dd512cb8ced01b1aa1
Melaku <melakubirhanu@gmail.com>
2022-12-15 09:31:51 -0800

    $msg

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - to print line
+ * @n: value accepted
+ * Return: return 0 on success
+ */
+	void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	break;
+
+	else
+	for (i = 0 ; i <= n; i++)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
+}
