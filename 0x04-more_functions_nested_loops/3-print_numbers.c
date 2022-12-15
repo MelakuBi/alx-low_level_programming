50b35cd0d9007b5d0aa3d09577f33260964d97a1
Melaku <melakubirhanu@gmail.com>
2022-12-15 08:40:58 -0800

    $msg

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - to print numbers
+ * Return: return 0 on success
+ */
+	void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9; i++)
+	{
+	_putchar("%d", i);
+	}
+}
