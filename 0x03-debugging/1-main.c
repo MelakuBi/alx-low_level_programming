b06ef3b6f12dd8326bdd1e565de5b5d6c6b5ee47
Melaku <melakubirhanu@gmail.com>
2022-12-15 14:00:34 -0800

    $msg

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,17 @@
+#include <stdio>
+/**
+ * main - check the code
+ * Return:0 
+ */
+int main(void)
+{
+	printf("Infinite loop incoming :(\n");
+	i = 0;
+
+	while (i < 0)
+	{
+	putchar(i);
+	}
+	printf("Infinifte loop avoided! \\o/\n");
+	return (0);
+}
