faadd411ded8b109ffb954e078ce7397b605381b
Melaku <melakubirhanu@gmail.com>
2022-12-15 10:36:47 -0800

    $msg

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,12 +8,6 @@
 	int i;
 	int j;
 
-	if (n <= 0)
-{
-	_putchar('\n');
-}
-	else
-{
 	for (i = 1; i < n; i++)
 	{
 	for (j = 1; j <= i; j++)
@@ -22,6 +16,5 @@
 	}
 	_putchar('\\');
 	_putchar('\n');
-}
 }
 }
