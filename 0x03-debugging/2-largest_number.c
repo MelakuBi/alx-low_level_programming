66ea97465b3441c8c74a5baa3001bce477228c97
Melaku <melakubirhanu@gmail.com>
2022-12-16 11:55:57 -0800

    $msg

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,19 +10,17 @@
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a > b && a > c)
 	{
 	largest = a;
 	}
-	else if (b > c && c > a)
+	else if (b >= a && b >= c)
 	{
 	largest = b;
 	}
-	else if (c > a && a > b)
+	else
 	{
 	largest = c;
 	}
-	else
-	largest = c;
 	return (largest);
 }
