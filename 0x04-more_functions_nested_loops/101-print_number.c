06db0a43072cfeb16bfcd5fcff04d8594b3148ba
Melaku <melakubirhanu@gmail.com>
2022-12-16 06:35:11 -0800

    $msg

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_numbers - to print intiger
+ * @n: variable to accept number
+ */
+	void print_numbers(int n)
+{
+	_putchar(n);
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -26,6 +26,7 @@
 	void print_triangle(int size);
 	void fizz_buzz(void);
 	int main(void);
+	void print_number(int n);
 
 
 #endif		
