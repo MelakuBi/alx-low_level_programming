19b6cfab08c77fb9456b1fa1a3bca2e1bde2dfba
Melaku <melakubirhanu@gmail.com>
2022-12-17 07:41:10 -0800

    $msg

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -23,12 +23,12 @@
 {
 	if (month == 2 && day == 60)
 	{
-		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+	printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 	}
 	else
 	{
-		printf("Daye of the year: %d\n", day);
-		printf("Reamaining days: %d\n", 365 - day);
+	printf("Day of the year: %d\n", day);
+	printf("Remaining days: %d\n", 365 - day);
 	}
 }
 }
