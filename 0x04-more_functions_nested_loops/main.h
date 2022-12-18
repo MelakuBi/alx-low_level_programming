06413c4c758f92af8bb1c7bcf37ab2fe5f185227
Melaku <melakubirhanu@gmail.com>
2022-12-18 10:42:24 -0800

    $msg

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-	int _putchar(char c);
+	int _putchar(int c);
 	void print_alphabet(void);
 	void print_alphabet_x10(void);
 	int _islower(int c);
@@ -17,7 +17,6 @@
 	int _isupper(int c);
 	int _isdigit(int c);
 	int mul(int a, int b);
-	void print_numbers(void);
 	void print_most_numbers(void);
 	void more_numbers(void);
 	void print_line(int n);
