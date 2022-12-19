f1a193b6b2db266988a7fe947f3cfa6313300a7e
Melaku <melakubirhanu@gmail.com>
2022-12-19 13:08:31 -0800

    $msg

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *puts2 - print only character
+ * @str: pinter
+ */
+	void puts2(char *str)
+{
+	int n;
+
+	for(i = 0; str[n] != '\0'; n++)
+{
+	if ((n % 2) == 0)
+	_putchar(str[n]);
+
+	else
+	continue;
+}
+	_putchart('\n');
+}
