5415671f479264238afa558614a20e20c1a8995d
Melaku <melakubirhanu@gmail.com>
2022-12-19 11:36:30 -0800

    $msg

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - this prints string in reverse
+ * @s: pointer to pass string value
+ */
+	void print_rev(char *s);
+{
+	char rev;
+	int i, len;
+
+	len = _strlen(s);
+	len = len - 1;
+	while (s[i] != '\0')
+	{
+	s[len] = s[i];
+	len--;
+	i++;
+	}
+}
+
