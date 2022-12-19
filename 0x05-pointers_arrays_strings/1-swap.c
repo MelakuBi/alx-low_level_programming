36b664244f0cf2e269c374689c0811be131f6eb5
Melaku <melakubirhanu@gmail.com>
2022-12-19 05:00:02 -0800

    $msg

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * swap_int - swap the value of two variables
+ * @a: the first pinter variable
+ * @b: the second pinter variable
+ */
+	void swap_int(int *a, int *b)
+{
+	a = &b;
+	b = &a;
+}
