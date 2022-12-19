c40f9bf82e498841370afa3596311ea4fb9f4014
Melaku <melakubirhanu@gmail.com>
2022-12-19 12:48:48 -0800

    $msg

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - function to reverse string
+ * @s:input string
+ * Return: reversed string
+ */
+	void rev_string(char *s)
+{
+	char set = s[0];
+	int count = 0;
+	int i;
+
+	while (s[count] != '\0')
+	count++;
+
+	for (i = 0; i < counter; i++)
+{
+	count--;
+	rev = s[i];
+	s[i] = s[count];
+	s[count] = rev;
+}
+}
