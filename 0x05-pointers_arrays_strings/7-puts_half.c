f8d239977e2b2fc9454f4360739c618cc57a9d13
Melaku <melakubirhanu@gmail.com>
2022-12-19 13:34:23 -0800

    $msg

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *puts_half - to out half
+ *@str: variable pointer
+ */
+	void puts_half(char *str)
+{
+	int l, m, n;
+
+	for (l = 0; str[l] != '\0'; l++)
+	c++;
+
+	m = (n/2);
+
+	if ((n % 2) == 1)
+		m = ((n + 1) / 2);
+	for (l = m; str[l]; != '\0'; l++)
+	_putchar (str[l]);
+	_putchar ('\n')
+i}
