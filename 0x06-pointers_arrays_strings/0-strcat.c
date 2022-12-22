c6382a4c8e22e031a3d91bceba0e4b25b14823ba
Melaku <melakubirhanu@gmail.com>
2022-12-22 08:25:10 -0800

    $msg

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_strcat - concatinate strings
+ * @dest: accept concatenated strings
+ * @src: second string
+ * Return: on success 0
+ */
+	char *_strcat(char *dest, char *src)
+{
+	int counter1 = 0, counter2 = 0;
+
+	while (dest[counter1] != '\0')
+	counter1++;
+
+	while (src[counter2] != '\0')
+	{
+	dest[counter1] = src[counter2];
+	counter1++;
+	counter2++;
+	}
+	return (0);
+}
