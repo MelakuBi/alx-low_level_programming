636b2c84fc4d4343ad6dcc88cfbb99cd8f651f1a
Melaku <melakubirhanu@gmail.com>
2022-12-22 12:38:18 -0800

    $msg

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reverse_array - function to reverse array contents
+ * @a: pointer to variable
+ * @n: number of tobe reverse
+ */
+	void reverse_array(int *a, int n)
+{
+	int i = 0, j = 0, m;
+
+	while (a[i] != '\0')
+	i++
+	for (; j <= n; j++)
+	{
+	m = a[j];
+	a[i] = m;
+	}
+}
