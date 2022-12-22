ce76f14f4c6c1f236a693bfe63ccee329aa613f3
Melaku <melakubirhanu@gmail.com>
2022-12-22 09:48:03 -0800

    $msg

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_strncat - concatinate strings and end at n
+ * @dest: accept concatenated strings
+ * @src: second string
+ * Return: pass dest to main function
+ */
+	char *_strncat(char *dest, char *src, int n)
+{
+	int counter1 = 0, counter2 = 0;
+
+	while (dest[counter1] != '\0')
+	counter1++;
+
+	while (src[counter2] <= n)
+	{
+	dest[counter1] = src[counter2];
+	counter1++;
+	counter2++;
+	}
+	return (dest);
+}
