4ddbc453562fbd2bbc995a9da17622b830477d02
Melaku <melakubirhanu@gmail.com>
2022-12-23 07:38:51 -0800

    $msg

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,15 +6,28 @@
  */
 	void reverse_array(int *a, int n)
 {
-	int i = 0, j, m;
+	int i = 0, j, m, l;
 
 	while (a[i] != '\0')
 	i++;
 
+	if (n != '\0')
+	{
 	for (j = 0; j < n; j++)
 	{
 	m = a[j];
 	a[i] = m;
 	i--;
+	}
+	}
+	else
+	{
+	l = i;
+	for (j = 0; j <= l; j++)
+	{
+	m = a[j];
+	a[i] = m;
+	i--;
+	}
 	}
 }
