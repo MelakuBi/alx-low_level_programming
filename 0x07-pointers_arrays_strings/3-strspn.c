b218392a167a05734b2ed56d214235bf8eb92c11
Melaku <melakubirhanu@gmail.com>
2023-01-02 14:09:21 -0800

    $msg

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * _strspn - gets the length of a prefix substring.
- *   * @s: character to print
- *    * @accept: character
- *     * Return: i.
+ * * @s: character to print
+ * * @accept: character
+ * * Return: i.
  */
 	unsigned int _strspn(char *s, char *accept)
 {
