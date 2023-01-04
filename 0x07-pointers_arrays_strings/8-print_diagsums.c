03ec0b01f11251fe09dd07038906e70a01a5a3c4
Melaku <melakubirhanu@gmail.com>
2023-01-04 12:28:12 -0800

    $msg

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -5,7 +5,7 @@
  * @a: pointer to an array
  * @size: size of
  * Return: void
- */
+*/
 	void print_diagsums(int *a, int size)
 {
 	int i, j;
@@ -14,16 +14,11 @@
 
 	for (i = 0; i < size; i++)
 {
-/*	if (size == 5)
-	{
-	c = c + (a + (i * 6));
-	}
-	else */
-	c = c + *(a + i * 4);
+	c = c + *(a + i * sizeof(int));
 }
 	for (j = 0; j < size; j++)
 {
 	d = d + *(a + (size * j) + (size - j - 1));
-}
+	}
 	printf("%d, %d\n", c, d);
 }
