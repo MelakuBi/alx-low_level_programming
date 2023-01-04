a0d75c74d548fb3373a14e8de65889b0ffa4ce5e
Melaku <melakubirhanu@gmail.com>
2023-01-04 11:27:05 -0800

    $msg

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - return string with new line
+ * @s: string
+ * Return: nuthing
+ */
+	void _print_rev_recursion(char *s)
+{
+	if (!*s)
+	return;
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
