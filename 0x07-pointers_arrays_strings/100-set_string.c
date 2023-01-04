a51b59ef43a50ef27560e95c9569d5fc2e3b6ce7
Melaku <melakubirhanu@gmail.com>
2023-01-04 12:44:01 -0800

    $msg

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -3,7 +3,7 @@
  * set_string - sets the value of a pointer to a char
  * @s: double pointer
  * @to: pointer
- * Return : Void function 
+ * Return : Void function
  */
 	void set_string(char **s, char *to)
 {
