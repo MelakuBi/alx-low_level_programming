827425903266d13b8547bd6da906e4c788b9a75a
Melaku <melakubirhanu@gmail.com>
2023-01-04 12:14:06 -0800

    $msg

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _pow_recursion - power
+ * @y: variable
+ * Return: integer
+ * @y : variable
+ */
+	int _pow_recursion(int t, int y)
+{
+	int pw = 1;
+	if (y < 0)
+	return (-1);
+	if (!y)
+	return (1);
+	pw = _pow_recursion(x, y - 1);
+	return (pw * x);
+}
+
