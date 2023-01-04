94c25b46e1cf6818dc889a72c596a27fe5f3d893
Melaku <melakubirhanu@gmail.com>
2023-01-04 12:05:05 -0800

    $msg

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * factorial(int n) - factorial 
+ * @n: number
+ * Return: okay
+ */
+{
+	int factorial(int n)
+	{
+	int fac = 1;
+
+	if (n < 0)
+	return (-1);
+	else if (!n)
+	return (1);
+	fac = factorial(n - 1);
+	return (fac * n);
+}
