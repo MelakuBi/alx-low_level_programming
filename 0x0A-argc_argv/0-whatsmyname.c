fcab3fe7d8fff069bacdd453bda880ee229317cf
Melaku <melakubirhanu@gmail.com>
2023-01-06 11:58:24 -0800

    $msg

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *  * main - program that prints its name.
- *   * @argc : number of arguments.
- *    * @argv : array that contains the arguments.
- *     * Return: 0.
+ * main - program that prints its name.
+ * @argc : number of arguments.
+ * @argv : array that contains the arguments.
+ * Return: 0.
  */
 	int main(int args, char *argv[])
 {
-	printf("Myname is &s\n", argv[0]);
-	(void)argc;
+	printf("&s\n", argv[0]);
 	return (0);
 }
