975259e6d9446171ac29ecac099dd5edc6e178d5
Melaku <melakubirhanu@gmail.com>
2023-01-06 12:21:41 -0800

    $msg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that prints its name.
+ * @argc : number of arguments.
+ * @argv : array that contains the arguments.
+ * Return: 0.
+ */
+	int main(int argc, char *argv[])
+{
+	printf("%s\n", argc);
+	(void)argv;
+	return (0);
+}
