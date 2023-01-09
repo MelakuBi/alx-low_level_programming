43d1e129c1f9251114802ad663a94026fe53b7d0
Melaku <melakubirhanu@gmail.com>
2023-01-09 12:02:37 -0800

    $msg

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+#include <limits.h>
+#include <stdlib.h>
+/**
+ *main - program that adds positive numbers
+ * @argv : array that contains the arguments
+ * @argc : counts number of passed arguments
+ * Return: on success 0
+ */
+	int main(int argc, char *argv[])
+{
+	int val, i, no;
+	int count = 0;
+
+	for (i = 1; i < argc; i++)
+	count++;
+	val = atoi(argv[1]);
+	(void)val;
+	if (count == 0 || count > 1)
+{
+	printf("Error\n");
+}
+	else if (*argv[1] =='-')
+	{
+	return (0);
+	printf("\n");
+	}
+	else
+	no = printf("%d\n", 3);
+	return (no);
+}
