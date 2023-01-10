fd42a592ac060553646b22b445dcca97cd5eb44d
Melaku <melakubirhanu@gmail.com>
2023-01-10 11:46:04 -0800

    $msg

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - a function that allocate memory
+ * @size: size of memory to be allocated
+ * @c: character variable to be stored
+ * Return: string value
+ */
+	char *create_array(unsigned int size, char c)
+{
+	char *str;
+	int i;
+
+	if (size == 0)
+	return (NULL);
+	str = malloc(sizeof(char) * size);
+	if (str == NULL)
+	return (NULL);
+	else
+	{
+	for (i = 0; i < size; i++)
+	str[i] = c;
+	return (str);
+	}
+}
