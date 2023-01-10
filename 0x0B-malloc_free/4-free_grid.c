8cf928590ce1916120c70da17d7504b7b5168eea
Melaku <melakubirhanu@gmail.com>
2023-01-10 12:32:06 -0800

    $msg

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *free_grid - free a two dimensional grid
+ * @grid: the given grid
+ * @height: the given hei
+ *      * Return: void
+ */
+	void free_grid(int **grid, int height)
+{
+	int i;
+
+	i = 0;
+	while (i < height)
+	{
+	free(grid[i]);
+	i++;
+	}
+	free(grid);
+}
