4a22b93c2f59efbe79d78c25c96edefb7661a527
MelakuBi <117753551+MelakuBi@users.noreply.github.com>
2023-01-11 16:46:08 +0300

    $msg

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid- a function allocate block of memory to array
+ * @width: inner array
+ * @height: outer array
+ * Return: pinter
+ */
+	int **alloc_grid(int width, int height)
+{
+	int *pointer;
+	int size;
+	
+	size = width * height;
+	pointer = calloc(size, sizeof(int);
+	if (size <= 0)
+	return (NULL);
+	else if (pinter == NULL);
+	return (NULL);
+	else
+	return (pointer);
+}
