81b1a6cf1b7e1dc496802e9b2540566de71268c9
MelakuBi <117753551+MelakuBi@users.noreply.github.com>
2023-01-11 17:05:36 +0300

    $msg

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,15 +8,15 @@
  */
 	int **alloc_grid(int width, int height)
 {
-	int *pointer;
+	int **pointer;
 	int size;
 	
 	size = width * height;
-	pointer = calloc(size, sizeof(int));
+	*pointer = calloc(size, sizeof(int));
 	if (size <= 0)
 	return (NULL);
 	else if (pointer == NULL)
 	return (NULL);
 	else
-	return (pointer);
+	return (*pointer);
 }
