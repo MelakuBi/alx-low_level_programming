fbb47518fe3c7ab1a2e76879ce08dbc5d4daf0a3
Melaku <melakubirhanu@gmail.com>
2023-01-14 06:05:55 -0800

    $msg

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@
 
 	pointer = &point;
 	size = width * height;
-	point = (int *)malloc(size * sizeof(int));
+	point = (int *)malloc(size * sizeof(int) + sizeof(char));
 	for (j = 0; j < height; j++)
 	{
 	point[j] = 0;
