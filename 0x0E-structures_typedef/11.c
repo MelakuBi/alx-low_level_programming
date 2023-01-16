277a307bd035e8cd75ca41ae779b30989e8b3b38
Melaku <melakubirhanu@gmail.com>
2023-01-16 05:42:35 -0800

    $msg

---
+++ 0x0E-structures_typedef/11.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *init_dog- initializing
+ *@d: pointer
+ *@name: the dog name
+ *@age: age of the dog
+ *@owner: owner of the dog
+ */
+	void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+	if (d != 0)
+	{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,7 +16,7 @@
 	char *owner;
 };
 
-/*void init_dog(struct dog *d, char *name, float age, char *owner);*/
+void init_dog(struct dog *d, char *name, float age, char *owner);
 typedef struct dog dog_t;
 int _putchar(char);
 
