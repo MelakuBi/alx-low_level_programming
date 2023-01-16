10c753d549bff3a3c28ae9a4929908e064a33620
Melaku <melakubirhanu@gmail.com>
2023-01-16 06:08:25 -0800

    $msg

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *print_dog- initializing
+ *@d: pointe
+ */
+	void print_dog(struct dog *d)
+{
+
+	if (d->name == NULL)
+	printf("name: (nil)");
+	if (d->age == 0)
+	printf("age: (nil)");
+	if (d->owner == NULL)
+	printf("owner: (nil)");
+	if (d == NULL)
+	exit;
+}
