4db5d5c0d9bd5f35c83a148a04067834d210a748
Melaku <melakubirhanu@gmail.com>
2023-01-16 05:44:28 -0800

    $msg

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,7 +10,7 @@
  */
 	void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
+/*	d = malloc(sizeof(struct dog));*/
 	if (d != 0)
 	{
 	d->name = name;
