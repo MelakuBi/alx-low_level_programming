d86b13963c48c2e5432f50b2f5ebf4c46760cd0d
Melaku <melakubirhanu@gmail.com>
2023-01-18 13:19:18 -0800

    $msg

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,7 +5,7 @@
  * print_name - function that prints a name
  * @name: name to print
  * @f: pointer to a function to use to print name
- *  Return: nothing
+ * Return: nothing
  */
 	void print_name(char *name, void (*f)(char *))
 {
