77db165cd2cad4961eb855621d3a2a8152951c36
Melaku <melakubirhanu@gmail.com>
2023-01-19 06:56:04 -0800

    $msg

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,7 +16,7 @@
 	if (n <= 0)
 	return (0);
 	va_start(ap, n);
-	for (i = 0; i <= 0; i++)
+	for (i = 0; i < n; i++)
 	a += va_arg(ap, int);
 	va_end(ap);
 	return (a);
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,14 @@
 #define _VARIADIC_FUNCTION_H_
 #include <stddef.h>
 
+struct type
+
+{
+	char *character;
+	void (*printf)(va_list);
+};
+typedef struct type variable_type;
+
 int _putchar(char c);
 
 int sum_them_all(const unsigned int n, ...);
@@ -11,4 +19,5 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char * const format, ...);
+
 #endif
