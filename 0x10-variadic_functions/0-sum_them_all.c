030972fe7a93aaf76f65062394017fcebc4fe51a
Melaku <melakubirhanu@gmail.com>
2023-01-19 06:51:21 -0800

    $msg

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,11 +13,11 @@
 	unsigned int i;
 	int a = 0;
 
-	if (n == 0)
+	if (n <= 0)
 	return (0);
 	va_start(ap, n);
 	for (i = 0; i <= 0; i++)
-	a = a + va_arg(ap, int);
+	a += va_arg(ap, int);
 	va_end(ap);
 	return (a);
 }
