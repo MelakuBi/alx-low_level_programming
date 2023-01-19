919cfafb1fdc52043c44fcefd65ec14d486bc8bc
Melaku <melakubirhanu@gmail.com>
2023-01-19 06:18:02 -0800

    $msg

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,19 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ *sum_them_all - add all passed arguments
+ *@n: first argument
+ *@...: ellipse
+ *Return: sum of all
+ */
+	int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int i, a;
+
+	va_start(ap, n);
+	for (i = n; i >= 0; i = va_arg(ap, int))
+	a = a + n;
+	return (a);
+}
