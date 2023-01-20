3eb06e69c44c1e93fea09a15f2d70ca4b000a399
Melaku <melakubirhanu@gmail.com>
2023-01-20 11:32:51 -0800

    $msg

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,5 @@
-#ifndef CALC_H
-#define CALC_H
+#ifndef 3-CALC_H
+#define 3-CALC_H
 
 int (*get_op_func(char *s))(int, int);
 
@@ -14,7 +14,6 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 
 /**
- *
  * struct op - Struct op
  * @op: The operator
  * @f: The function asociated
