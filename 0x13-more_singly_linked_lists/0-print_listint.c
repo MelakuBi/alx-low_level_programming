7ee715df8bdae3a1a85e05cb43b892c3e611bc8c
Melaku <melakubirhanu@gmail.com>
2023-01-31 11:07:39 -0800

    $msg

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include <list.h>
+/**
+ *print_listint - prints list of linkdlist and their number
+ *@h : its a pointer point to head and traverse to next
+ *Return : returns number of data
+ */
+	size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+	return (0);
+	while (h != NULL)
+	{
+	printf("%d\n", h->n);
+	h = h->next;
+	count ++;
+	}
+	return (count);
+}
