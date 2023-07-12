3eb62c509c75a98fd5fbea9a2c9b6a121748fc17
MelakuBi <MelakuBirhanu@gmail.com>
2023-07-12 12:55:11 -0700

    $msg

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,7 +11,7 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-size_t i;
+	size_t i;
 
 	if (array == NULL)
 	{
@@ -19,11 +19,11 @@ size_t i;
 	}
 	for (i = 0; i < size; i++)
 	{
-		    printf("Value checked array[%lu] = [%i]\n", i, array[i]);
-		    if (value == array[i])
-		    {
-                    return (i);         
-	    	}
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (value == array[i])
+		{
+			return (i);
+		}
 	}
 	return (-1);
 }
